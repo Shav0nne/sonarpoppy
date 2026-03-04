d482f74ab8cffcc90485acd336783e586632f39d
Ishika <esoebhag@gmail.com>
2026-03-04 10:10:47 +0100

    Backend Opstart Basis

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/index.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//CORS MIDDLEWARE
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+//accept header middleware
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        return next();
+    }
+    const accept = req.headers.accept;
+
+    if (!accept || !accept.includes("application/json")) {
+        return res.status(406).json({
+            message: "Only application/json is allowed in Accept header"
+        });
+    }
+    next();
+});
+
+// Routes
+app.use("/", router);
+
+// Database connectie
+try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB");
+} catch (error) {
+    console.error("MongoDB connection error", error);
+}
+
+app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Server is listening on port ${process.env.EXPRESS_PORT}`);
+});
